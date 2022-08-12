78a60b7b9a9ada999828530cd89b57da51368c20
Lionnel Patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2022-08-12 12:55:29 +0000

    Initial commit

---
+++ src/main/java/sn/finetech/finelunch/FinelunchApplication.java
@@ -0,0 +1,13 @@
+package sn.finetech.finelunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinelunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinelunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/finetech/finelunch/FinelunchApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.finetech.finelunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinelunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
